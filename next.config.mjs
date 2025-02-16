bce3ba6f294691d027ac37cd59de04059c093f30
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2025-02-17 03:41:38 +0500

    fixing

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'i.ibb.co',
-      },
-    ],
-  },
+        hostname: 'i.ibb.co'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
